40cb4d4249e7cec88fc38ae8ab20cfcb2a60c270
fabio <fabio@fabiopozzi.net>
2024-05-16 17:20:40 +0200

    agg repository elpa

--- emacs/init.el
+++ emacs/init.el
@@ -35,6 +35,7 @@
 (setq package-enable-at-startup nil)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")))
 ;;(package-initialize)
 
