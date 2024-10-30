5c7e98b135a11eb3515232acd8425fa35310cb56
fabio <fabio@fabiopozzi.net>
2024-10-30 16:34:09 +0100

    Remove evil configuration and evil keybindings

--- emacs/init.el
+++ emacs/init.el
@@ -110,31 +110,6 @@
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
-;(use-package evil
-  ;:init
-  ;(setq evil-want-integration t)
-  ;(setq evil-want-keybinding nil)
-  ;(setq evil-want-C-u-scroll t)
-  ;(setq evil-want-C-i-jump nil)
-  ;:config
-  ;(evil-mode 1)
-  ;(define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  ;(define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-
-  ;;; Use visual line motions even outside of visual-line-mode buffers
-  ;(evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  ;(evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-  ;(evil-set-initial-state 'messages-buffer-mode 'normal)
-  ;(evil-set-initial-state 'dashboard-mode 'normal))
-
-;(use-package evil-collection
-  ;:after evil
-  ;:config
-  ;(evil-collection-init))
-
-;(evil-set-undo-system 'undo-redo)
-
 (use-package neotree
   :ensure t
   :config
@@ -146,23 +121,6 @@
 
 (global-set-key (kbd "C-x 2") 'ic/split-and-follow-horizontally)
 (global-set-key (kbd "C-x 3") 'ic/split-and-follow-vertically)
-
-;; custom evil keybindings
-
-;; set leader key in tutti gli stati
-;(evil-set-leader nil (kbd "SPC"))
-;;; tasti vari
-;;(evil-define-key 'normal 'global (kbd "<leader>fs") 'save-buffer)
-;(evil-define-key 'normal 'global (kbd "<leader>b") 'list-buffers)
-;(evil-define-key 'normal 'global (kbd "<leader>g") 'magit-status)
-
-(use-package hydra)
-
-(defhydra hydra-text-scale (:timeout 4)
-  "scale text"
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
-  ("f" nil "finished" :exit t))
 
 (use-package perspective
   :bind
