f037b04b464a1c2e6560425e66a1a546cde3e5ee
fabio <fabio@fabiopozzi.net>
2025-01-22 23:37:04 +0100

    Agg. package corfu e dabbrev per completamento (da configurare)

--- emacs/init.el
+++ emacs/init.el
@@ -164,7 +164,7 @@
   :ensure t
   :bind
   (("C-<iso-lefttab>" . tab-line-switch-to-prev-tab)
-   ("C-<tab>" . tab-line-switch-to-next-tab)))
+  ("C-<tab>" . tab-line-switch-to-next-tab)))
 
 (use-package vterm
     :ensure t)
@@ -177,6 +177,39 @@
 
 (use-package solarized-theme
   :config (load-theme 'solarized-dark t))
+
+;; completion using corfu (da configurare)
+(use-package corfu
+  ;; Optional customizations
+  ;; :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+
+  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :init
+  (global-corfu-mode))
+
+;; Use Dabbrev with Corfu!
+(use-package dabbrev
+  ;; Swap M-/ and C-M-/
+  :bind (("M-/" . dabbrev-completion)
+         ("C-M-/" . dabbrev-expand))
+  :config
+  (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
+  ;; Since 29.1, use `dabbrev-ignored-buffer-regexps' on older.
+  (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
+  (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode))
 
 (use-package emacs
   :config
